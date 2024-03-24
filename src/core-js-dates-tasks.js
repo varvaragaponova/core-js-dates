fe991c62c2e4ae8a736ccb347753599412d0e5a7
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2024-03-24 18:54:23 +0400

    feat: add solution for getNextFridayThe13th task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -251,8 +251,16 @@ function getWeekNumberByDate(/* date */) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const currentDate = new Date(date);
+  currentDate.setDate(13);
+  for (let i = currentDate.getMonth(); i < 12; i += 1) {
+    currentDate.setMonth(i);
+    if (currentDate.getDay() === 5) {
+      break;
+    }
+  }
+  return currentDate;
 }
 
 /**
